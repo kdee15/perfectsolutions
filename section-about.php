18069ec5f637f314151b01fb3caffce8bc52b674
kirk <kdee15@gmail.com>
2020-02-12 00:04:41 +0200

    Update section-about.php

--- section-about.php
+++ section-about.php
@@ -8,15 +8,44 @@
     <div class="row">
       <article class="col-12 offset-md-6 col-md-6">
         <p>
-          Leading from both a strategic and operational perspective, the consultancy ensures that services are specifically tailored to the clients’ needs, while ensuring project outcomes are managed through effective project management.
+          Perfect Solutions Events and Promotions PTY LTD registration number:  2013/057375/07 trading as Perfect Solutions Through-The-Line Marketing (PS TTL). PS TTL was established in 2008 as a through-the-line marketing agency. The agency boasts 12 years of success of bringing brands and consumers together through innovative concepts, underpinned by a 360-degree approach to delivering marketing services to clients. Most of the clients the agency services, are global brands.
         </p>
-          The consultancy provides highly competent project teams for projects of all sizes and complexities. The provision of project teams ranges from multi-skilled teams to a single project consultant.
+        During the latter part of its existence, the agency has expanded to establish Perfect Solutions Investment Holdings as the umbrella company, with PS TTL now being one of the subsidiary companies.
         </p>
         <p>
-          Perfect Solutions Project Management Consultancy believes in transferring expert project management skills to build in-house capacity of organizations. Project management training and development is offered as an additional service to clients’ that need to enhance their project teams. Project management workshops are designed to capacitate project teams with project management fundamentals.
+          The expansion of Perfect Solutions across South Africa and Central East Africa has enabled the business to settle in several African countries. Entrepreneurship is highlighted as an important tool to that drive's sustainable economic growth in emerging and developing economies across Africa.
+        </p>
+        <p>
+          African countries are now turning to entrepreneurship as an alternative strategy to grow their economies and decrease unemployment. Perfect Solutions aims to be part of harnessing this huge untapped potential which is driving the continent forward to solve some of Africa’s greatest challenges.
         </p>
       </article>
       <div class="o-block-sidebar"></div>
     </div>
   </div>
+
+  <div class="m-title-block">
+    <div class="container">
+      <h2>The journey of expanding the business is based on</h2>
+      <h2>four fundamental pillars</h2>
+    </div>
+  </div>
+
+  <div class="container o-content-container">
+    <div class="row">
+      <aside class="m-aside puzzle-piece piece-1">1</aside>
+      <aside class="m-aside puzzle-piece piece-2">2</aside>
+    </div>
+    <div class="row">
+      <aside class="m-aside puzzle-piece piece-3">3</aside>
+      <aside class="m-aside puzzle-piece piece-4">4</aside>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
 </section>
