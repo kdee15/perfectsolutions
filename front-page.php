591e1e3db9a649659ab258b1828c9866bf86940b
Kirk Daniels <kirk@lab.co.uk>
2020-02-25 10:47:02 +0200

    companies

--- front-page.php
+++ front-page.php
@@ -41,7 +41,6 @@ get_header();
 
   <?php get_template_part( 'section-values' ); ?>
   <?php get_template_part( 'section-timeline' ); ?>
-  <?php get_template_part( 'section-companies' ); ?>
   <?php get_template_part( 'section-brands' ); ?>
   <?php get_template_part( 'section-contact' ); ?>
 
