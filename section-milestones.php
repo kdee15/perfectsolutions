ffa66f8991269d731c014ca34f9aceecca82ebbb
kirk <kdee15@gmail.com>
2020-03-05 00:08:36 +0200

    Update section-milestones.php

--- section-milestones.php
+++ section-milestones.php
@@ -4,7 +4,6 @@
   </div>
 
   <div class="container-fluid">
-    <div class="row no-gutters">
 
     <?php
 
@@ -15,40 +14,45 @@
     ) );
 
     foreach($terms as $term) {
-    echo "<div class='m-milestone-tab'>";
-    echo "<span class='a-milestone-tab-title'>" . $term->name . "</span>'";
-    echo "</div>";
+      echo "<div class='o-milestone-year'>";
+        echo "<div class='row no-gutters'>";
+          echo "<div class='m-milestone-tab'>";
+            echo "<span class='a-milestone-tab-title'>" . $term->name . "</span>'";
+          echo "</div>";
+        echo "</div>";
+        echo "<div class='row no-gutters'>";
 
-      $args=array(
-        'post_type' => 'milestones',
-        'post_status' => 'publish',
-        'taxonomy' => 'year_group',
-        'order' => 'DESC',
-        'posts_per_page' => 3
-      );
-      $my_query = null;
-      $my_query = new WP_Query($args);
+          $args=array(
+            'post_type' => 'milestones',
+            'post_status' => 'publish',
+            'order' => 'DESC',
+            'posts_per_page' => 3
+          );
+          $my_query = null;
+          $my_query = new WP_Query($args);
 
-      if( $my_query->have_posts() ) {
-        while ($my_query->have_posts()) : $my_query->the_post(); ?>
+          if( $my_query->have_posts() ) {
+            while ($my_query->have_posts()) : $my_query->the_post(); ?>
 
-          <article class="col-12 col-sm-6 col-lg-4 card-milestone">
-            <div class="o-card milestone-card">
-              <div class="m-card-image" style="background-image:url('<?php echo get_the_post_thumbnail_url($post->ID, 'single-post-thumbnail'); ?>');">
-              </div>
-              <div class="m-card-body">
-                <h3 class="a-title"><?php the_title(); ?></h3>
-                <?php the_content(); ?>
-              </div>
-            </div>
-          </article>
-        <?php endwhile;
-      }
-      wp_reset_query();  // Restore global post data stomped by the_post().
-    }
-    ?>
+              <article class="col-12 col-sm-6 col-lg-4 card-milestone">
+                <div class="o-card milestone-card">
+                  <div class="m-card-image" style="background-image:url('<?php echo get_the_post_thumbnail_url($post->ID, 'single-post-thumbnail'); ?>');">
+                  </div>
+                  <div class="m-card-body">
+                    <h3 class="a-title"><?php the_title(); ?></h3>
+                    <?php the_content(); ?>
+                  </div>
+                </div>
+              </article>
+            <?php endwhile;
+          }
+        echo "</div>";
+      echo "</div>";
+          wp_reset_query();  // Restore global post data stomped by the_post().
+        }
+        ?>
+      </div>
     </div>
-  </div>
 
   <div class="row m-cta-wrapper m-see-all d-none">
     <span class="a-cta-text">See All</span>
